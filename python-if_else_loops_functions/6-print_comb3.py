220d3b98025aee5d1dd282fdcd995af2ba6ba2ec
Romina Guimaraens <5071@holbertonstudents.com>
2022-09-13 05:15:17 -0300

    vs save error

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,3 @@
 for i in range(0, 9):
     for j in range(i + 1, 10):
         print("{}{}".format(i, j), end=("\n" if (i == 8 and j == 9) else ", "))
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=("\n" if (i == 8 and j == 9) else ", "))
