639cce0de23b019691fe291372fc5805c9582c9f
Romina Guimaraens <5071@holbertonstudents.com>
2022-09-22 13:10:21 -0300

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" an empty square class
+"""
+
+
+class Square:
+    """A class that definses a square
+    """
+    __init__(self, size):
+        self.__size = size
+    """Initializes size
+    """
