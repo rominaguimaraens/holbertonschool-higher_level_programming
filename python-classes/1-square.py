ba3c0f229afecfa3494c545b8ebbac494cf85c3c
Romina Guimaraens <5071@holbertonstudents.com>
2022-09-22 13:18:16 -0300

    pep8 fix

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,9 +7,9 @@ class Square:
     """A class that definses a square
     """
     def __init__(self, size):
-        """__init__ 
+        """__init__
         __init__ initializes size
-        Attributes: 
+        Attributes:
             size (int): size of the square
         """
         self.__size = size
