c19032aadfba432d43ba0cc4596b25d478a3a94c
Romina Guimaraens <5071@holbertonschool.com>
2022-09-25 14:12:16 -0700

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" a square class
+"""
+
+
+class Square:
+    """A class that definses a square
+    """
+    def __init__(self, size=0):
+        """__init__
+        __init__ initializes size
+        Attributes:
+            size (int): size of the square
+        """
+        self.__size = size
+
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def size(self):
+        return self.__size
+
+    def area(self):
+        """area
+        the area of the square
+        """
+        area = self.__size * self.__size
+        return area
