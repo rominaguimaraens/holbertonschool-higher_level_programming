825be93dabd1541f919aeca392c55b10a68e1d3d
Romina Guimaraens <5071@holbertonstudents.com>
2022-10-08 18:19:50 -0300

    pep8 fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Python Interpreter"""
 
+
 class Base:
     """Base object class
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is None:
             Base.__nb_objects += 1
