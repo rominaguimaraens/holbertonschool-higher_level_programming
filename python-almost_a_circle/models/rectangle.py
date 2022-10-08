02f0a191a1d841e621b312796dd65a011a578488
Romina Guimaraens <5071@holbertonstudents.com>
2022-10-08 18:33:58 -0300

    task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Python Interpreter"""
+
+
+from models.base import Base
+
+class Rectanlge(Base):
+    """ Rectangle class from base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super.__init__(id)
+        self.width = width
+        self.height = height
+
+    @property
+    def width(width):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(height):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+    
+    @property
+    def x(x):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(y):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
