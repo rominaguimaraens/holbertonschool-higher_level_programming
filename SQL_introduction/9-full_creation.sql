8bb6b65c333c4e884538b0a103a8df3a8ce24a50
rominaguimaraens <5071@holbertonstudents.com>
2022-11-06 15:22:50 -0300

    fix.

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,13 @@
--- create full table
+-- creates full table
 CREATE TABLE IF NOT EXISTS second_table(
-    'id' INT,
-    'name' VARCHAR(265),
-    'score' INT
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
 );
-INSERT INTO 'second_table'('id', 'name', 'score') VALUES(1, "John", 10);
-INSERT INTO 'second_table'('id', 'name', 'score') VALUES(2, "Alex", 3);
-INSERT INTO 'second_table'('id', 'name', 'score') VALUES(3, "Bob", 14);
-INSERT INTO 'second_table'('id', 'name', 'score') VALUES(4, "George", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
