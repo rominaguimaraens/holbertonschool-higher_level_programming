19b230aaadb2acedd10e6f71dab8dfca96ebb706
Romina Guimaraens <5071@holbertonstudents.com>
2022-11-09 18:31:05 -0300

    indent fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """Python interpreter"""
-
-"""Import module"""
 import MySQLdb
 from sys import argv
+
 
 """sql query"""
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost"
-        port=3306
-        user=argv[1]
-        password=argv[2]
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
         db_name=argv[3]
-    )
-
-cursor = db.cursor()
-cursor.execute(
-    SELECT id, name FROM states ORDER BY states.id ASC;
-)
+        )   
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT id, name FROM states ORDER BY states.id ASC;"
+        )
