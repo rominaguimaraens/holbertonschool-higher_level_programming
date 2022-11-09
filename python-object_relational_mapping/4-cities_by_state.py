d0ae1bf69ce3c21cf8cbb7e00a95df4f7d2da1fd
Romina Guimaraens <5071@holbertonstudents.com>
2022-11-09 19:51:32 -0300

    join states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
         )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cursor.execute("""SELECT cities.id, cities.name, states.name 
+    FROM cities JOIN states ON cities.state_id = states.id 
+    ORDER BY cities.id ASC""")
     result = cursor.fetchall()
 
     for row in result:
