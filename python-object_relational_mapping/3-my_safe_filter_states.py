708d78d335f37fcb24293abd100f565502275b01
Romina Guimaraens <5071@holbertonstudents.com>
2022-11-09 19:23:50 -0300

    format delete

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
         )
 
     cursor = db.cursor()
-    cursor.execute('''SELECT * FROM states WHERE name LIKE %s
-    ORDER BY states.id ASC;'''.format(argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (argv[4]))
     result = cursor.fetchall()
 
     for row in result:
