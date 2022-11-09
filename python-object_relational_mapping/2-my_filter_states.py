f338217eb93f1ee980e80e98b96a9b0bb101560d
Romina Guimaraens <5071@holbertonstudents.com>
2022-11-09 19:04:02 -0300

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python interpreter"""
+import MySQLdb
+from sys import argv
+
+
+"""sql query"""
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+        )
+    cursor = db.cursor()
+    cursor.execute('''SELECT * FROM states WHERE name LIKE argv[4]
+    ORDER BY states.id ASC;''')
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
