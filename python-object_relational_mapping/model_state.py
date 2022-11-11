24d361a7cba173f66e691ec99eb08be77a771d50
Romina Guimaraens <5071@holbertonstudents.com>
2022-11-11 20:11:56 -0300

    different way

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-""" State model """
+"""
+    State is a class that inherits from Base, and has an id and name column.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
 
-import sys
-from model_state import Base, State
+Base = declarative_base()
 
-from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+class State(Base):
+    """
+        State Class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
