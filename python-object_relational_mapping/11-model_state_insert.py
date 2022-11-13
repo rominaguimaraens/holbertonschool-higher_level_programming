d20812974c38ee273486b2fa1614a592c08b6256
rominaguimaraens <5071@holbertonstudents.com>
2022-11-13 20:49:34 -0300

    task 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,27 @@
-test
+#!/usr/bin/python3
+"""Python interpreter"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+        )
+    except Exception:
+        print("Error")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    lou = State(name='Louisiana')
+    ses.add(lou)
+    ses.commit()
+    cont = ses.query(State).order_by(State.id)\
+        .filter(State.name == 'Louisiana').all()
+    if cont:
+        print(f"{cont[0].id}")
+    ses.close()
