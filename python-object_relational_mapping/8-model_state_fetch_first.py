694c1aba4c666b1169469c7adc97193c84a29af1
rominaguimaraens <5071@holbertonstudents.com>
2022-11-13 19:41:05 -0300

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,26 @@
-test
+#!/usr/bin/python3
+"""Python interpreter"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """aaaa"""
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+        )
+    except Exception:
+        print("Error")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    cont = ses.query(State).order_by(State.id).first()
+    if cont:
+        print(f"{cont.id}: {cont.name}")
+    else:
+        print("Nothing")
+    ses.close()
