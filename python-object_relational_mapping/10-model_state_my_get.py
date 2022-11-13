853a8ece094425afd8c96912574de4befb01280f
rominaguimaraens <5071@holbertonstudents.com>
2022-11-13 20:19:02 -0300

    task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,26 @@
-test
+#!/usr/bin/python3
+"""Python interpreter"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+        )
+    except Exception:
+        print("Error")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    cont = ses.query(State).order_by(State.id)\
+        .filter(State.name == argv[4]).all()
+    if cont:
+        print(f"{cont[0].id}")
+    else:
+        print("Not found")
+    ses.close()
