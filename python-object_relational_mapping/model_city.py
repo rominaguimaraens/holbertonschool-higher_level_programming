6b1c7c249a350a924d6dfa270990310d92d08df0
rominaguimaraens <5071@holbertonstudents.com>
2022-11-13 21:48:09 -0300

    m

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,13 @@
-model
+#!/usr/bin/python3
+"""Python interpreter"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
