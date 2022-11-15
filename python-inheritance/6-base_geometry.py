a90415bfcf88fb104b5778033cd78ca227a66cf6
Romina Guimaraens <5071@holbertonschool.com>
2022-11-15 08:18:49 -0800

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python interpreter"""
+
+
+class BaseGeometry:
+    """Empty class"""
+
+    def area(self):
+        """Area"""
+        raise Exception("area() is not implemented")
