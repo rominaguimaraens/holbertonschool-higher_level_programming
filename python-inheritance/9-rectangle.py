1610948a4cf62b2f712983510547fc7bee37a149
Romina Guimaraens <5071@holbertonschool.com>
2022-11-15 08:35:33 -0800

    doc fix

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Python interpreter"""
+"""Module"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """New child class"""
+    """new child class"""
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
 
     def __init__(self, width, height):
-        """Initializes a new instance"""
+        """initializes new instance"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """Methos that calculates the Area"""
-        return self.__width * self.
+        """method returns area of rectangle"""
+        return self.__width * self.__height
