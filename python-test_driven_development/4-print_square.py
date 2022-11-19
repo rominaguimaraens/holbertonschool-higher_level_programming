5d409203b10ac51fb94580d5d08f619a9966681f
rominaguimaraens <5071@holbertonstudents.com>
2022-11-19 15:46:19 -0300

    task 3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""prints a square"""
+
+
+def print_square(size):
+    """border cases"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is not float and size < 0:
+        raise TypeError("size must be an integer")
+    for rows in range(size):
+        for cols in range(size):
+            print("#", end='' if cols != size - 1 else '\n')
