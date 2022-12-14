2f733eaa85c04dcf264af153add931bc412e1836
Romina Guimaraens <5071@holbertonstudents.com>
2022-12-14 17:57:52 -0300

    task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myVar = []; let counter = 0;
+myVar.push('C is fun', 'Python is cool', 'JavaScript is amazing');
+
+while (counter < myVar.length) {
+  console.log(myVar[counter]);
+  counter++;
+}
